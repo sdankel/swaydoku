a3a3b468fdcecc62f595f7548164b921c6ac2313
Sophie <47993817+sdankel@users.noreply.github.com>
2024-12-21 05:08:46 -0800

    check for window

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { FuelProvider } from '@fuels/react';
 import { defaultConnectors } from '@fuels/connectors';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -9,11 +9,17 @@ import { queryClient } from './queryClient';
 import PuzzleBoard from './Board';
 
 export default function Home() {
+  const connectors = useMemo(() => {
+    if (typeof window !== 'undefined') {
+      return defaultConnectors({ devMode: true });
+    }
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <FuelProvider
         fuelConfig={{
-          connectors: defaultConnectors({ devMode: true }),
+          connectors,
         }}>
         <PuzzleBoard />
       </FuelProvider>
